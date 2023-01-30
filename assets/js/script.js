71986e0c636410cd8cc217fdb8a3995ac9a29fbf
mariuml <mariumladha97@gmail.com>
2023-01-30 11:49:50 +0000

    Got today's weather to display on click of search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,11 @@
-
-// Connect input from the search bbox to the searched variable  
-
-
 // Click event
 
 $("#search-button").click(function (event){
-
 
 //  Prevent default
 event.preventDefault();
 
 // Grab text from input box
-
 var cityName = $("#search-input").val();
 
 // Connecting to the city search API
@@ -24,23 +18,25 @@ var cityQueryURL =
 
 // Connecting to the 5-day weather app API
 
-var latitude = 0;
-var longitude = 0;
-var weatherQueryURL =
+// AJAX function: Getting the longitude and latitude from city input
+$.ajax({
+  url: cityQueryURL,
+  method: "GET",
+}).then(function (response) {
+var latitude = response[0].lat;
+var longitude = response[0].lon;
+
+ console.log(latitude);
+ console.log(longitude);
+
+
+ var weatherQueryURL =
   "https://api.openweathermap.org/data/2.5/forecast?lat=" +
   latitude +
   "&lon=" +
   longitude +
   "&appid=" +
   apiKey;
-
-// AJAX function: Getting the longitude and latitude from city input
-$.ajax({
-  url: cityQueryURL,
-  method: "GET",
-}).then(function (response) {
-latitude = response[0].lat;
- longitude = response[0].lon;
 
 
 //   AJAX function: Getting the weather using the coodinates
@@ -50,8 +46,26 @@ latitude = response[0].lat;
 }).then(function (response) {
 console.log(response);
 
+// Display API results on the page 
+
+// Show City Name
+$("#selected-city-heading").text(cityName);
+
+// Show current temperature
+var todaysTemp = (Math.floor((response.list[0].main.temp) -273.15));
+$("#selected-city-temp").text("Temp: " + todaysTemp + "°C");
+
+// Show wind speed
+var todaysWind = response.list[0].wind.speed;
+$("#selected-city-wind").text("Wind: " + todaysWind + " KPH");
+console.log(todaysWind);
+// Show humidity 
+var todaysHumidity = response.list[0].main.humidity;
+$("#selected-city-humidity").text(" Humidity: " + todaysHumidity + " %");
+
 });
 });
 });
+
 
 
