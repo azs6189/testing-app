3a067afd2833b98bd1470fc93d0de3117c5fe435
mariuml <mariumladha97@gmail.com>
2023-01-30 13:04:38 +0000

    Saved today's search to local storage and got it to render on page after refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,43 @@
+
+// Retreiving:
+
+function displayLocalStorage (){
+// City name:
+    $("#selected-city-heading").text(window.localStorage.getItem("selectedCity")
+    + " " + "(" + 
+    (moment().format("L")) +
+    ")");
+    
+
+// Today's temperature:
+$("#selected-city-temp").text(
+    "Temp: " +
+    (window.localStorage.getItem("todaysTemp") +
+    "°C"
+    ));
+
+// Today's wind:
+$("#selected-city-wind").text(
+    "Wind: " + 
+    (window.localStorage.getItem("todaysWind")) +
+    " KPH");
+    
+
+// Today's humidity
+$("#selected-city-humidity").text(
+    " Humidity: " +
+    (window.localStorage.getItem("todaysHumidity")) +
+    " %"
+    
+    );
+}
+
+// Getting information from local storage and displaying to page if it exists
+if (localStorage.getItem("selectedCity") !== null) {
+    displayLocalStorage();
+}
+
+
 // Click event
 
 $("#search-button").click(function (event){
@@ -26,10 +66,6 @@ $.ajax({
 var latitude = response[0].lat;
 var longitude = response[0].lon;
 
- console.log(latitude);
- console.log(longitude);
-
-
  var weatherQueryURL =
   "https://api.openweathermap.org/data/2.5/forecast?lat=" +
   latitude +
@@ -46,22 +82,31 @@ var longitude = response[0].lon;
 }).then(function (response) {
 console.log(response);
 
-// Display API results on the page 
+// Display today's API results on the page - this needs to be saved to local storage to remain on the page 
 
-// Show City Name
-$("#selected-city-heading").text(cityName);
+// Show City Name from local storage and today's date with moment
+var todaysDate = moment().format("L");
+localStorage.setItem("selectedCity", cityName);
+$("#selected-city-heading").text(cityName + " " + "(" + todaysDate + ")");
+
 
 // Show current temperature
 var todaysTemp = (Math.floor((response.list[0].main.temp) -273.15));
+localStorage.setItem("todaysTemp", todaysTemp);
 $("#selected-city-temp").text("Temp: " + todaysTemp + "°C");
 
 // Show wind speed
 var todaysWind = response.list[0].wind.speed;
+localStorage.setItem("todaysWind", todaysWind);
 $("#selected-city-wind").text("Wind: " + todaysWind + " KPH");
-console.log(todaysWind);
+
+
 // Show humidity 
 var todaysHumidity = response.list[0].main.humidity;
+localStorage.setItem("todaysHumidity", todaysHumidity);
 $("#selected-city-humidity").text(" Humidity: " + todaysHumidity + " %");
+
+// Display 5-day forecast 
 
 });
 });
